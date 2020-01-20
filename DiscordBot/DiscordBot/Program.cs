8d4f1d285325362e7d0b5ea0377af5454aca916f
ReinKaas <reinsurewaard@hotmail.com>
2020-01-20 11:47:02 +0100

    CodeBot

---
+++ DiscordBot/DiscordBot/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Discord;
+using Discord.WebSocket;
+using System.Threading.Tasks;
+
+namespace DiscordBot
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        => new Program().StartAsync().GetAwaiter().GetResult();
+        
+        private DiscordSocketClient _client;
+
+        public async Task StartAsync()
+        {
+            _client = new DiscordSocketClient();
+
+            await _client.LoginAsync(TokenType.Bot, "NjY4NzQ1MDk2NzY1NzY3Njgx.XiWEag.5QswZGPHXcaYDDrQLj6BNr2_etM");
+
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+    }
+}
